b959d6f075541e453dab9a0b3a673290d5abbfe9
William James Ngana <williamjamesngana@Williams-MacBook-Pro.local>
2021-08-09 15:36:23 -0400

    first commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,78 @@
+import os
+import requests
+import re 
+import yake
+from flask import Flask, request
+
+
+
+gMap = {
+    "Acoustic Blues":1210,
+    "Chicago Blues": 1007,
+    "Blues": 2,
+    "Rap": 18,
+}
+
+search_baseUrl ='http://api.musixmatch.com/ws/1.1/track.search?'
+lyrics_baseURL = 'http://api.musixmatch.com/ws/1.1/track.lyrics.get?'
+
+def get_five_words(lyrics,language):
+    max_ngram_size = 3
+    deduplication_threshold = 0.9
+    numOfKeywords = 5
+    custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
+    keywords = custom_kw_extractor.extract_keywords(lyrics)
+    return [x[0] for x in keywords]
+
+def get_tracks(category,songno,alreadyplayed,token):
+    if songno == 0:
+        response = requests.get(
+            search_baseUrl+ f'f_has_lyrics=1&f_music_genre_id={gMap[category]}&page=1&page_size=2&apikey={token}'
+        ).json()
+        return response
+    else:
+        track_id = alreadyplayed[0].split(',')[-1]
+        lyrics = requests.get(
+            lyrics_baseURL+ f'track_id={track_id}&apikey={token}'
+        ).json().get('message').get('body').get('lyrics')
+        keywords = get_five_words(lyrics.get('lyrics_body'),lyrics.get('lyrics_language','en'))
+        response = requests.get(
+            search_baseUrl+ f'f_has_lyrics=1&f_music_genre_id={gMap[category]}&page=1&page_size=1&q_lyrics={keywords}&apikey={token}'
+        ).json()
+        return response
+    
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/playlist', methods=['GET'])
+    def get_songs():
+        if request.method == 'GET':
+            category = request.args.get('category', default = '*', type = str)
+            songno = request.args.get('songno', default = 0, type = int)
+            alreadyplayed = request.args.getlist("alreadyplayed")
+            token = request.args.get('token', default = '*' ,type = str)
+            return get_tracks(category,songno,alreadyplayed,token)
+        else:
+            return "Bad Request", 400
+
+    return app
